04b07c479a846212a3c9c885ab1e9d07ae1afe1d
Java-Monsta <TheBourneCode@gmail.com>
2022-02-21 04:20:49 -0600

    Uploading latest CodeWars completed.

---
+++ 2022-02-21.js
@@ -0,0 +1,17 @@
+// Code Wars
+
+// Title:
+// Remove String Spaces
+
+// Description:
+// Simple, remove the spaces from the string, then return the resultant string.
+
+function noSpace(x) {
+  let newStr = ''
+  for(let i = 0; i < x.length; i++) {
+    if(x[i] !== " "){
+      newStr += x[i]
+    }
+  }
+  return newStr
+}
