c67dc66224af42e231e64bf10be363a280c24a25
Russell M <99734077+Java-Monsta@users.noreply.github.com>
2022-02-25 01:03:01 -0600

    Uploading latest CodeWars completed

---
+++ 2022-02-25.js
@@ -0,0 +1,18 @@
+// Code Wars
+
+// Title:
+// Convert a Boolean to a String
+
+// Description
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+
+function booleanToString(b){
+  if(b) {
+    return "true";
+  } else {
+    return "false";
+  }
+}
