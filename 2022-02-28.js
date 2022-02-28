77d537dfa342d9d5411c04e3028690334c9b59ee
Russell M <99734077+Java-Monsta@users.noreply.github.com>
2022-02-28 01:16:08 -0600

    Uploading latest CodeWars completed

---
+++ 2022-02-28.js
@@ -0,0 +1,15 @@
+// Code Wars
+
+// Title:
+// Beginner - Lost Without a Map
+
+// Description
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+function maps(x){
+  return x.map(n => n * 2);
+}
