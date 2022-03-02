0f41de6bbfd59dd8faaa9b8320fd0ebe30d6dade
Russell M <99734077+Java-Monsta@users.noreply.github.com>
2022-03-02 01:19:05 -0600

    Uploading latest CodeWars completed

---
+++ 2022-03-02.js
@@ -0,0 +1,11 @@
+// Code Wars
+
+// Title:
+// You Can't Code Under Pressure #1
+
+// Description
+// Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+  return i * 2;
+}
