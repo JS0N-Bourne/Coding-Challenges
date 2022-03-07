aac8759d433e9328491b249f0983643959a842ef
Russell M <99734077+Java-Monsta@users.noreply.github.com>
2022-03-07 05:51:32 -0600

    Uploading latest CodeWars completed

---
+++ 2022-03-07.js
@@ -0,0 +1,22 @@
+// Code Wars
+
+// Title:
+// MakeUpperCase
+
+// Description
+// Write a function which converts the input string to uppercase.
+
+
+PREP
+
+PARAMETERS
+
+RETURNS
+
+EXAMPLES
+
+PSEUDOCODE
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
