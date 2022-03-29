447005a38e1e733c48fd70b374a04028e430a6cb
Russell M <99734077+Java-Monsta@users.noreply.github.com>
2022-03-28 21:33:01 -0500

    Uploading latest CodeWars completed

---
+++ 2022-03-28.js
@@ -0,0 +1,22 @@
+// Code Wars
+
+// Title:
+// Function 3 - multiplying two numbers
+
+// Description
+// Implement a function which multiplies two numbers.
+
+
+PREP
+
+PARAMETERS
+
+RETURNS
+
+EXAMPLES
+
+PSEUDOCODE
+
+function multiply(a, b) {
+  return a * b;
+  }
