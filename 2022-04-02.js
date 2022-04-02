fbf7f9184a71f0959e9634fc76caf83747dfe08e
Russell M <99734077+Java-Monsta@users.noreply.github.com>
2022-04-02 05:24:22 -0500

    Uploading latest CodeWars completed

---
+++ 2022-04-02.js
@@ -0,0 +1,27 @@
+// Code Wars
+
+// Title:
+// Volume of a Cuboid
+
+// Description
+// Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+
+PREP
+
+PARAMETERS
+
+cube = x * y * z
+
+RETURNS
+
+EXAMPLES
+
+PSEUDOCODE
+
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return (length * width * height);
+  }
+}
