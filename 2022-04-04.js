b953de10ba4a63dda88b9f77e6fe50b683d1f0f2
Russell M <99734077+Java-Monsta@users.noreply.github.com>
2022-04-04 05:56:48 -0500

    Uploading latest CodeWars completed

---
+++ 2022-04-04.js
@@ -0,0 +1,31 @@
+// Code Wars
+
+// Title:
+// Grasshopper - Personalized Message
+
+// Description
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+// Use conditionals to return the proper message:
+
+// case	return
+// name equals owner	'Hello boss'
+// otherwise	'Hello guest'
+
+
+PREP
+
+PARAMETERS
+
+RETURNS
+
+EXAMPLES
+
+PSEUDOCODE
+
+function greet (name, owner) {
+  if (name == owner) {
+    return "Hello boss";
+  }
+    return "Hello guest";
+}
