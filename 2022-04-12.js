ba20434486f944e11a0de9d33f788a933237d606
Russell M <99734077+Java-Monsta@users.noreply.github.com>
2022-04-12 15:59:29 -0500

    Uploading latest CodeWars completed

---
+++ 2022-04-12.js
@@ -0,0 +1,20 @@
+// Code Wars
+
+// Title:
+// Remove exclamation marks
+
+// Description
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+
+PARAMETERS
+
+RETURNS
+
+EXAMPLES
+
+PSEUDOCODE
+
+function removeExclamationMarks(s) {
+  return s.replace(/!/g,'');
+}
