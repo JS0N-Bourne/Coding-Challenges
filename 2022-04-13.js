e1701415f9192aef4855b69b94a95c847d224b15
Russell M <99734077+Java-Monsta@users.noreply.github.com>
2022-04-13 16:01:01 -0500

    Uploading latest CodeWars completed

---
+++ 2022-04-13.js
@@ -0,0 +1,28 @@
+// Code Wars
+
+// Title:
+// Third Angle of a Triangle
+
+// Description
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+
+PARAMETERS
+given 2 angles of a triangle
+
+RETURNS
+return 3rd angle
+3rd angle is calculated by 180 - (a + b)
+
+EXAMPLES
+
+PSEUDOCODE
+
+
+function otherAngle(a, b) {
+  return 180 - (a + b);
+}
