1fb592b7de33df21ed29ecea0b45344238e0b6e0
Russell M <99734077+Java-Monsta@users.noreply.github.com>
2022-04-15 16:16:44 -0500

    Uploading latest CodeWars completed

--- 2022-04-15.js
+++ 2022-04-15.js
@@ -1,36 +1,39 @@
 // Code Wars
 
 // Title:
-// Find the first non-consecutive number
+// Quarter of the year
 
 // Description
-// ]
+// Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+// For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
 
 
 PREP
 
-PARAMETERS
+PARAMETERS 
+* given a number 
+* decide which quarter (1/1-3/31, 4/1-6/30, 7/1-9/30,10/1-12/31) it falls in
 
 RETURNS
+return quarterOf as integer number
 
 EXAMPLES
+ if given number 4, expect a return of "2"
+ if given number 8, expect a return of "3"
 
 PSEUDOCODE
 
-Your task is to find the first element of an array that is not consecutive.
-
-By not consecutive we mean not exactly 1 larger than the previous element of the array.
-
-E.g. If we have an array [1,2,3,4,6,7,8] then 1 then 2 then 3 then 4 are all consecutive but 6 is not, so that's the first non-consecutive number.
-
-If the whole array is consecutive then return null2.
-
-The array will always have at least 2 elements1 and all elements will be numbers. The numbers will also all be unique and in ascending order. The numbers could be positive or negative and the first non-consecutive could be either too!
-
-
-function firstNonConsecutive(arr) {
-  for (let i = 1; i < arr.length; i++) {
-      if (arr[i - 1] + 1 !== arr[i]) return arr[i];
+const quarterOf = (month) => {
+  if (month <= 3) {
+    return 1;
+  }else if(month <= 6) {
+    return 2;
+  } else if (month <= 9) {
+    return 3;
+  } else if (month <= 12) {
+    return 4;
   }
-  return null;
-}
+
+  }
+
