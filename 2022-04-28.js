13b098052241c8d4124edff17f6ec13b322719cc
Java-Monsta <TheBourneCode@gmail.com>
2022-04-29 01:39:12 -0500

    Several CodeWards 8kyu challenges in one file

---
+++ 2022-04-28.js
@@ -0,0 +1,40 @@
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+function sumOfNums(arr) {
+  return arr.reduce((acc,c) => acc + (c > 0 ? c : 0),0);
+}
+
+
+// Given an array of integers your solution should find the smallest integer.
+
+// For example:
+
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+function findMinimum(args) {
+  return args.reduce((acc, c) => acc < c ? acc : c)
+}
+
+// Nathan loves cycling.
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+// For example:
+
+// time = 3 ----> litres = 1
+
+// time = 6.7---> litres = 3
+
+// time = 11.8--> litres = 5
+
+function litres(time) {
+  return Math.floor(time / 2)
+}
