ffbc5f2357c2c315f66e0518e43b4d6392fa5bb3
Russell M <99734077+Java-Monsta@users.noreply.github.com>
2022-05-01 19:31:12 -0500

    Uploading latest CodeWars completed

---
+++ 2022-05-1.js
@@ -0,0 +1,27 @@
+// Code Wars
+
+// Title:
+// Twice as old
+
+// Description
+// Your function takes two arguments:
+
+// current father's age (years)
+// current age of his son (years)
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+
+PREP
+
+PARAMETERS
+
+RETURNS
+
+EXAMPLES
+
+PSEUDOCODE
+
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  let twice = dadYearsOld - sonYearsOld * 2;
+  return twice < 0 ? twice * (-1) : twice;
+}
