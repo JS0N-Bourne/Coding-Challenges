0710fc98557ad3e4f0e54448015e903944a700e0
Russell M <99734077+Java-Monsta@users.noreply.github.com>
2022-05-04 19:36:59 -0500

    Uploading latest CodeWars completed

---
+++ 2022-05-04.js
@@ -0,0 +1,39 @@
+// Code Wars
+
+// Title:
+// Grasshopper - Messi Goals
+
+// Description
+// Use variables to find the sum of the goals Messi scored in 3 competitions
+
+// Information
+// Messi goal scoring statistics:
+
+// Competition	Goals
+// La Liga	43
+// Champions League	10
+// Copa del Rey	5
+// Task
+// Create these three variables and store the appropriate values using the table above:
+// laLigaGoals
+// championsLeagueGoals
+// copaDelReyGoals
+// Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+
+
+PREP
+
+PARAMETERS
+
+RETURNS
+
+EXAMPLES
+
+PSEUDOCODE
+
+
+var laLigaGoals = 43;
+var championsLeagueGoals = 10;
+var copaDelReyGoals = 5;
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
