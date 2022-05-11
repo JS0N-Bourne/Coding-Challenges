e56adec034f8f33b294d6e620cc06dd1260f115c
Russell M <99734077+Java-Monsta@users.noreply.github.com>
2022-05-10 19:44:52 -0500

    Uploading latest CodeWars completed

---
+++ 2022-05-10.js
@@ -0,0 +1,24 @@
+// Code Wars
+
+// Title:
+// Expressions Matter
+
+// Description
+// Task
+Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained
+
+
+PREP
+
+PARAMETERS
+
+RETURNS
+
+EXAMPLES
+
+PSEUDOCODE
+
+function expressionMatter(a, b, c) {
+  return Math.max(a * (b + c), a * b * c, a + b + c, (a + b) * c);
+}
