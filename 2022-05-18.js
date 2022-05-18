e428883ef9de89127eb57b80d27e9273de926f79
Russell M <99734077+Java-Monsta@users.noreply.github.com>
2022-05-18 18:32:18 -0500

    Uploading latest CodeWars completed

---
+++ 2022-05-18.js
@@ -0,0 +1,20 @@
+// Code Wars
+
+// Title:
+// Function 2 - squaring an argument
+
+// Description
+// Now you have to write a function that takes an argument and returns the square of it.
+
+
+PREP
+
+PARAMETERS
+
+RETURNS
+
+EXAMPLES
+
+PSEUDOCODE
+
+const square = number => number ** 2;
