c3400aa69e670feaa92bb79f703425d5517c755d
Russell M <99734077+Java-Monsta@users.noreply.github.com>
2022-05-21 18:37:24 -0500

    Uploading latest CodeWars completed

---
+++ 2022-05-21.js
@@ -0,0 +1,30 @@
+// Code Wars
+
+// Title:
+// Unfinished Loop - Bug Fixing #1
+
+// Description
+// Unfinished Loop - Bug Fixing #1
+// Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+
+
+PREP
+
+PARAMETERS
+
+RETURNS
+
+EXAMPLES
+
+PSEUDOCODE
+
+
+function createArray(number){
+  var newArray = [];
+  
+  for(var counter = 1; counter <= number; counter++){
+    newArray.push(counter);
+  }
+  
+  return newArray;
+}
